3b5ab848517c0c64cabae8b75399ab4ad1bbc510
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2025-09-17 00:25:39 +0500

    chore: setup the prettier and format all the file

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch("/api/hello")
+    fetch('/api/hello')
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
-      .catch((error) => console.error("Error fetching message:", error));
+      .catch((error) => console.error('Error fetching message:', error));
   }, []);
 
-  return <p>{message}</p>;
+  return (
+    <div className="p-4">
+      <h2 className="text-3xl font-semibold">Hello World</h2>
+      <p>{message}</p>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
 const PORT = 3000;
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from the API!" });
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
 });
 
 app.listen(PORT, () => {
