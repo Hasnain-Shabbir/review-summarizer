4bace294c178a3763b832f8746ee9ada2db7f5cb
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2025-09-16 23:43:57 +0500

    chore: add the dotenv package to load the env variables

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
 const PORT = 3000;
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
