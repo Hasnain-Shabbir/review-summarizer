23ce73a6e41c6f401688ab691844a0cb3454c981
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2025-09-21 16:31:54 +0500

    update: add the schema validation on the chatbot

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 const app = express();
 app.use(express.json());
@@ -22,8 +23,24 @@ app.get('/api/hello', (req, res) => {
 
 const conversations = new Map<string, string>();
 
+// Always apply min and max to secure the token usage.
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .trim()
+    .min(1, 'Prompt is required')
+    .max(1000, 'Prompt is too long (max 100 characters)'),
+  conversationId: z.string().uuid(),
+});
+
 // api endpoint to communicate with the OpenAI API
 app.post('/api/chat', async (req: Request, res: Response) => {
+  const parseResult = chatSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    res.status(400).json({ error: parseResult.error.format() });
+    return;
+  }
+
   const { prompt, conversationId } = req.body;
 
   const response = await client.responses.create({
