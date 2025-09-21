3075d4519777195f309957afe007029d251cf68d
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2025-09-21 16:21:16 +0500

    update: add the map to maintain the chat history

---
+++ notes.md
@@ -0,0 +1,3 @@
+- To keep the memory, we can use a variable to store the id of last chat.
+
+- We can use a map to store the chat id for different chats to keep history.
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -20,16 +20,21 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the API!' });
 });
 
+const conversations = new Map<string, string>();
+
 // api endpoint to communicate with the OpenAI API
 app.post('/api/chat', async (req: Request, res: Response) => {
-  const { prompt } = req.body;
+  const { prompt, conversationId } = req.body;
 
   const response = await client.responses.create({
     model: 'gpt-4o-mini',
     input: prompt,
     temperature: 0.2, // we need accurate responses, and for chat we can go with .2
     max_output_tokens: 100,
+    previous_response_id: conversations.get(conversationId),
   });
+
+  conversations.set(conversationId, response.id);
 
   res.json({
     message: response.output_text,
